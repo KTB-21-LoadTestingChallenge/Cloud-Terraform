1589674ba7a79c23d202d047ea1579aafadd8e9e
LyleSeungju <rlatmdwn31@gmail.com>
2024-12-10 19:19:25 +0900

    add ssh key

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
+  key_name               = "lt-kakao-lyle"
   
   root_block_device {
     volume_size = var.volume
